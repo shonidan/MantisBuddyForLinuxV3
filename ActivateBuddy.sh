92e26021b92a0cf00444958de2f010df8ddd7736
C0MODIN <38263591+C0MODIN@users.noreply.github.com>
2023-03-25 18:21:01 -0300

    Update to V3

--- ActivateBuddy.sh
+++ ActivateBuddy.sh
@@ -1,71 +1,67 @@
 #!/bin/bash
+
 adb_port="5555"
-msgTitle="MantisBuddy Activation Tool"
+msgTitle="MantisBuddyForLinuxV3"
 dirPath="/sdcard/Android/data/app.mantispro.gamepad/files"
-
-
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 
 adb kill-server
 adb disconnect
 
+BTN_RETURNED=""
 
-function detect(){
-
-    if [ `adb devices|grep "unauthorized"|wc -l` -ge 1 ]
+detect() {
+    adb devices | sed -n '2p' > output.txt | sed -n '1p' output.txt > output.txt
+    statusDevice=$(awk 'NR==1{print $NF}' output.txt)
+    echo $statusDevice
+    if [ "$statusDevice" = "unauthorized" ]
     then phoneUnauthorized
-         return
+        return
     fi
 
-    $DIR/adb root
-    if [ $? -eq 1 ]
+    if [ -z $statusDevice ]
     then phoneNotFound
-         return
+        return
     fi
 
-    if [ $? -eq 0 ]
-    then inject
-         return
+    if [ "$statusDevice" = "device" ]; then
+        inject
+        return
     fi
+    rm output.txt
 }
 
-function inject(){
+inject() {
+    adb shell find "$dirPath/buddyNew.sh"
 
-    $DIR/adb shell find $dirPath/buddyNew.sh
-
-    if [ $? -eq 1 ]
-    then titleStr="Please Launch App 'Mantis Gamepad Pro' on your Device and click Retry."
-         notify-send -t 0 "$titleStr"
-         echo -e "$titleStr"
-
-    else $DIR/adb shell sh $dirPath/buddyNew.sh
-         succes_msg_1='🎮 Activation Complete!'
-         succes_msg_2='Please Launch "Mantis Gamepad Pro", then open MantisBuddy Screen and check if Activated.\nIf Activation fails after Unplugging, please run ActivateBuddyWifi.sh.'
-         notify-send -t 0 "$succes_msg_1" "$succes_msg_2"
-         echo -e "$succes_msg_1,\n$succes_msg_2"
+    if [ "$?" -eq 1 ]; then
+        titleStr="Please Launch App 'Mantis Gamepad Pro' on your Device and click Retry."
+        btnReturned "$titleStr" inject
+    else
+        adb shell sh "$dirPath/buddyNew.sh"
+        zenity --info --title="MantisBuddy Activation Tool" --text="Activation Complete!!\nPlease Launch 'Mantis Gamepad Pro', then open MantisBuddy Screen and check if Activated. If Activation fails after Unplugging, please run GamepadProWifiBuddy.command."
     fi
     exit
-
 }
 
-function phoneNotFound(){
-
-    titleStr='Android Device Not Found!!'
-    descriptionStr='Make sure your devices USB Debugging is On. Reconnect your phone to PC via USB cable and retry'
-    echo -e "$titleStr,\n$descriptionStr"
-    notify-send -t 0 "$titleStr" "$descriptionStr"
-
+phoneNotFound() {
+    titleStr="Android Device Not Found!! \n\n1. Make sure your devices's USB Debugging is On; \n2. Reconnect your phone to PC via USB cable\n3. Click Retry."
+    btnReturned "$titleStr" detect
 }
 
-function phoneUnauthorized(){
+phoneUnauthorized() {
+    titleStr="Tap Allow button on device's USB Debugging popup window and click Retry. \n \n If popup not shown, click Cancel and run me again.."
+    btnReturned "$titleStr" detect
+}
 
-    titleStr='Tap Allow button on device USB Debugging popup window and retry.'
-    descriptionStr='If popup not shown, click Cancel and run me again..'
-    echo -e "$titleStr,\n$descriptionStr"
-    notify-send -t 0 "$titleStr" "$descriptionStr"
-
+btnReturned() {
+    BTN_RETURNED=$(zenity --question --title=$msgTitle --text="$1" --ok-label=Retry --cancel-label=Quit --default-cancel)
+    OUTPUT=$?
+    if [ "$OUTPUT" -eq 0 ]
+    then detect
+    elif [ "$OUTPUT" -eq 1 ]
+    then exit
+    else return
+    fi
 }
 
 detect
