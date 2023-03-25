263dc09039ea91939ea2a8670dba5c26abcd8e80
C0MODIN <38263591+C0MODIN@users.noreply.github.com>
2023-03-25 19:31:38 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 ActivateBuddy="ActivateBuddy.sh"
-ActivateBuddyWifi="ActivateBuddyWifi.sh"
 chmod +x "$ActivateBuddy"
-chmod +x "$ActivateBuddyWifi"
 sudo cp ActivateBuddy.sh /usr/bin/ActivateBuddy
-sudo cp ActivateBuddyWifi.sh /usr/bin/ActivateBuddyWifi
 echo -e "Installation succesfully"
